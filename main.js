24eda5d9791d5986b98dce69ff75d26c013fd61a
Asadiftekhar <143116178+asad755@users.noreply.github.com>
2024-03-30 19:41:51 +0500

    todo.proj

---
+++ main.js
@@ -0,0 +1,57 @@
+export {};
+/*
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+
+while(condition){
+let addTask = await inquirer.prompt(
+    [
+    }
+        name:'todo',
+        type:'input',
+        message:"what you want to add in your Todos?"
+    },
+{
+        name:'addMore',
+        type:'confirm',
+        message:"Do you want to add more?"
+        default: "false"
+}
+    ]
+
+);
+todos.push(addTask.todo);
+condition = addTask.addMore;
+console.log(todos)
+
+
+/*
+import inquirer from "inquirer";
+
+let todos: string[] = ["sahri", "fasting", "playing"];
+
+let condition = true;
+
+//while(condition) {
+    let addTask = await inquirer.prompt([
+        //{
+            []
+            name: 'todo',
+            type: 'input',
+            message: "What do you want to add to your Todos?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "Do you want to add more?",
+            default: false
+        }
+    ]);
+
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+
+    console.log(todos);
+}   */
